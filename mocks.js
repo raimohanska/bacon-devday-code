8ae5f116bee1cc8ba45055581327d77cfababf65
Juha Paananen <juha.paananen@gmail.com>
2012-09-23 15:45:16 +0300

    initial commit

---
+++ mocks.js
@@ -0,0 +1,9 @@
+$.mockjax({
+  url: '/usernameavailable/*',
+  dataType: 'json',
+  response: function(settings) {
+    var username = settings.url.match(/.+\/(\w+$)/)[1]
+    var available = username.length % 2 == 0
+    this.responseText = available;
+  }
+});
